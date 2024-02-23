07bcf08eac44bdf5308f43f3551bfdf0c97967e2
Sukhbeer Singh Sasan <sukhbeers12@gmail.com>
2024-02-23 13:52:26 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,8 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+
+var isValid = function(str) {
+    const stack = []; 
+    const pairs = { 
+        "(": ")", 
+        "[": "]", 
+        "{": "}",
+    }; 
+  
+    for (let char of str) { 
+        if (pairs[char]) { 
+            stack.push(char); 
+        } else if ( char === ")" ||  char === "]" || char === "}"){ 
+            if ( 
+                pairs[stack.pop()] !== char 
+            ) { 
+                return false; 
+            } 
+        } 
+    } 
+  
+    return stack.length === 0; 
 };
 
 module.exports = { isValid };
